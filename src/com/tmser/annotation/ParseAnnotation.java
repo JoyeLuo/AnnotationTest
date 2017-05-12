ccdf8e3a8609dd27f23bad5f9afbc2a706d1eaf8
Joye Luo <joye.luo@liferay.com>
2017-05-12 14:17:54 +0800

    java-annotation-tutorial

---
+++ src/com/tmser/annotation/ParseAnnotation.java
@@ -0,0 +1,64 @@
+
+package com.tmser.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+public class ParseAnnotation
+{
+
+    public static void parseTypeAnnotation() throws ClassNotFoundException
+    {
+        Class clazz = Class.forName( "com.tmser.annotation.UserAnnotation" );
+
+        Annotation[] annotations = clazz.getAnnotations();
+        for( Annotation annotaion : annotations )
+        {
+            TestA testA = (TestA) annotaion;
+            System.out.println( "id=" + testA.id() + ";" + "name=" + testA.name() + ";" + "gid=" + testA.gid() );
+        }
+    }
+
+    public static void parseMethodAnnotation()
+    {
+        Method[] methods = UserAnnotation.class.getDeclaredMethods();
+
+        for( Method method : methods )
+        {
+            boolean hasAnnotation = method.isAnnotationPresent( TestA.class );
+            if( hasAnnotation )
+            {
+                TestA annotation = method.getAnnotation( TestA.class );
+                System.out.println(
+                    "method=" + method.getName() + ";" + "id=" + annotation.id() + ";" + "description=" +
+                        annotation.name() + ";gidid=" + annotation.gid() );
+            }
+        }
+    }
+
+    public static void parseConstructAnnotation()
+    {
+        Constructor[] constructors = UserAnnotation.class.getConstructors();
+
+        for( Constructor constructor : constructors )
+        {
+            boolean hasAnnotation = constructor.isAnnotationPresent( TestA.class );
+            if( hasAnnotation )
+            {
+                TestA annotation = (TestA) constructor.getAnnotation( TestA.class );
+                System.out.println(
+                    "constructor = " + constructor.getName() + " ; id = " + annotation.id() + " ; description = " +
+                        annotation.name() + "; gid= " + annotation.gid() );
+            }
+        }
+    }
+
+    public static void main( String[] args ) throws ClassNotFoundException
+    {
+        parseTypeAnnotation();
+        parseMethodAnnotation();
+        parseConstructAnnotation();
+    }
+
+}
